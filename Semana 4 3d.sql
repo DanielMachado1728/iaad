3b3a6cb3645802f0b8be9483a0bb0d739cb5f287
DanielMachado1728 <54379990+DanielMachado1728@users.noreply.github.com>
2020-10-11 14:22:09 -0300

    Update Semana 4 3d.sql

--- Semana 4 3d.sql
+++ Semana 4 3d.sql
@@ -1,7 +1,3 @@
 UPDATE DEPARTAMENTO
-SET Cpf_gerente = '12345678966'
-where Dnumero = 5;
-
-UPDATE DEPARTAMENTO
-SET Data_inicio_gerente = '2020-09-07'
+SET Cpf_gerente = '12345678966', Data_inicio_gerente = '2020-09-07'
 where Dnumero = 5;
