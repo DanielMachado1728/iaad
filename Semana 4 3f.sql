ab237160442aa8188443770f36ce4351271be0bf
DanielMachado1728 <54379990+DanielMachado1728@users.noreply.github.com>
2020-10-11 13:59:39 -0300

    Update Semana 4 3f.sql

--- Semana 4 3f.sql
+++ Semana 4 3f.sql
@@ -1,3 +1,3 @@
 UPDATE Funcionario
 SET salario = salario - (salario*0.1)
-where (select Dnumero from DEPARTAMENTO where Dnome = 'Administração') = Funcionario.Dnr;
+where (select Dnumero from DEPARTAMENTO where Dnome = 'Administração') = Dnr;
