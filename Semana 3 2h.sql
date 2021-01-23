728a8c03fc37c35a2c449e5f919f4444702d8ac0
DanielMachado1728 <54379990+DanielMachado1728@users.noreply.github.com>
2021-01-23 15:48:59 -0300

    Update Semana 3 2h.sql

--- Semana 3 2h.sql
+++ Semana 3 2h.sql
@@ -1,4 +1,9 @@
-select P.Projnome, D.Dnome, count(*), Sum(Horas)
+CREATE VIEW informacao_projeto(nome_projeto, nome_departamento, numero_funcionarios, horas)
+as select P.Projnome, D.Dnome, count(*), Sum(Horas)
 from PROJETO as P, DEPARTAMENTO as D, TRABALHA_EM as T
 where D.Dnumero = P.Dnum and P.Projnumero = T.Pnr
 group by Pnr;
+
+-- Consulta para testar a view
+select nome_projeto, nome_departamento, numero_funcionarios, horas
+from informacao_projeto;
